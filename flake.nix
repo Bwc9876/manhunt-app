0d6317a0a65ae80407dd3fb104e7d6c6cfa68363
Ben C <bwc9876@gmail.com>
2025-05-06 13:04:45 -0400

    Setup Nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flakelight.url = "github:nix-community/flakelight";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { flakelight, ... } @ inputs:
     flakelight ./. {
       inherit inputs;
+      withOverlays = [ inputs.rust-overlay.overlays.default ];
+      nixpkgs.config = {
+        allowUnfree = true;
+        android_sdk.accept_license = true;
+      };
+
       formatters =
         let
           forAllTypes = cmd: types:
@@ -20,13 +28,40 @@
           "*.rs" = "cd backend; cargo fmt";
         }
         // (forAllTypes "prettier --write ." [ "ts" "tsx" "md" "json" ]);
-      devShell = {
-        shellHook = pkgs: ''
-          export XDG_DATA_DIRS="$GSETTINGS_SCHEMAS_PATH"
-          export GIO_EXTRA_MODULES="${pkgs.dconf.lib}/lib/gio/modules:${pkgs.glib-networking}/lib/gio/modules"
-        '';
-        packages = pkgs:
-          with pkgs; [
+      devShell = pkgs:
+        let
+          buildToolsVersion = "34.0.0";
+          androidComposition = pkgs.androidenv.composeAndroidPackages {
+            platformVersions = [
+              "34"
+              "latest"
+            ];
+            systemImageTypes = [ "google_apis_playstore" ];
+            buildToolsVersions = [ buildToolsVersion ];
+            abiVersions = [
+              "armeabi-v7a"
+              "arm64-v8a"
+              "x86_64"
+            ];
+            includeNDK = true;
+            includeExtras = [
+              "extras;google;auto"
+            ];
+          };
+        in
+        {
+          shellHook =
+            let
+              ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
+            in
+            ''
+              export XDG_DATA_DIRS="$GSETTINGS_SCHEMAS_PATH"
+              export GIO_EXTRA_MODULES="${pkgs.dconf.lib}/lib/gio/modules:${pkgs.glib-networking}/lib/gio/modules"
+              export ANDROID_HOME=${ANDROID_HOME}
+              export NDK_HOME="${androidComposition.androidsdk}/libexec/android-sdk/ndk/${builtins.head (pkgs.lib.lists.reverseList (builtins.split "-" "${androidComposition.ndk-bundle}"))}"
+              export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2"
+            '';
+          packages = with pkgs; [
             at-spi2-atk
             atkmm
             cairo
@@ -42,10 +77,11 @@
             pkg-config
             gobject-introspection
             nodePackages.prettier
-            cargo
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override { targets = [ "aarch64-linux-android" "armv7-linux-androideabi" "i686-linux-android" "x86_64-linux-android" ]; }))
             cargo-tauri
             nodejs
+            (android-studio.withSdk androidComposition.androidsdk)
           ];
-      };
+        };
     };
 }
