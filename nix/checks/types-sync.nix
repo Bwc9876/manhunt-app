2a141893303d5dca734d07ce3e1cd315875b1f1f
Ben C <bwc9876@gmail.com>
2025-06-15 19:04:46 -0400

    Fix checks

--- nix/checks/types-sync.nix
+++ nix/checks/types-sync.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:
 pkgs.runCommand "check-types-synced" {} ''
   ${pkgs.manhunt}/bin/export-types ./bindings.ts
-  ${pkgs.prettier}/bin/prettier --write ./bindings.ts --config ${../../frontend/.prettierrc.yaml}
+  ${pkgs.prettier}/bin/prettier --write ./bindings.ts --config ${../../.prettierrc.yaml}
   diff bindings.ts ${../../frontend/src/bindings.ts}
   touch $out
 ''
